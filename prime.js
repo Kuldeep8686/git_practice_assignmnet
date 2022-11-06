195f5c4d2f4cc173482d0174862b1e46746ef673
Kuldeep Kumar Baitha <kuldeepkumar83986@gmail.com>
2022-11-06 22:00:25 +0530

    prime file added

---
+++ prime.js
@@ -0,0 +1,51 @@
+  let num = 13;
+   for(let i=num;i<=i;i++){
+     if(num%i==0)
+       count++;
+   }
+  
+     if(count==3){
+       console.log("No. is prime");
+    } else {
+        console.log("No. is not prime");
+       }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+5
+
+
+
+
+
+
+
+
+
+
+
+
+5
+
+    
